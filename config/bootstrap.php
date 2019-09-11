edb6f6e78fc6d4ec6ed808614065dd0485aabd46
Chris Rowles <cmrowles@pm.me>
2019-09-11 23:51:51 +0100

    all

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,6 +8,7 @@ require __DIR__.'/../vendor/autoload.php';
 /*----------------------------------------
  | Load environment variables             |
  ----------------------------------------*/
+ 
 
 /*----------------------------------------
  | Load application dependencies          |
