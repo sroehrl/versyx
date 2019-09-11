ca8037710d8165ffefcc6be6a44134a70afe2d05
Chris Rowles <cmrowles@pm.me>
2019-09-12 00:19:30 +0100

    modified route

--- config/routes.php
+++ config/routes.php
@@ -5,5 +5,5 @@
  ----------------------------------------*/
 /** @var \App\Controllers\HomeController $home */
 $app['router']->respond('GET', '/', function () use ($home) {
-    return $home->view(['title' => 'Home']);
+    return $home->view(['title' => 'Chris Rowles | Software Developer']);
 });
